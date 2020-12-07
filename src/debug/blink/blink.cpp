404b1f99b997b9d47064ab12ac65ddc287061b32
Sebastian Kopf <sebkopf>
2020-12-07 16:29:08 -0700

    make debug/blink work in manual mode

--- src/debug/blink/blink.cpp
+++ src/debug/blink/blink.cpp
@@ -2,19 +2,28 @@
 
 // generic blink test
 
-int led = D7; // This one is the little blue LED on your board. On the Photon it is next to D7, and on the Core it is next to the USB jack.
-int interval = 500; // how many ms to wait between blinks
+// constants
+const int led = D7; // This one is the little blue LED on your board. On the Photon it is next to D7, and on the Core it is next to the USB jack.
+const unsigned int interval = 1000; // how many ms to wait between blinks
+byte mac[6];
 
+// process variables
+long unsigned int timer = 0;
+bool on = true;
+bool connect = false;
+bool connected = false;
 
+// device info
 char device_name[20];
 char device_public_ip[20];
 
+// info handler
 void info_handler(const char *topic, const char *data) {
     if ( strcmp (topic, "spark/device/name") == 0) {
-        Serial.println("Device name: " + String(data));
+        Serial.println("Found device name: " + String(data));
         strncpy ( device_name, data, sizeof(device_name) );
     } else if ( strcmp(topic, "spark/device/ip") == 0) {
-        Serial.println("Device public IP: " + String(data));
+        Serial.println("Found device public IP: " + String(data));
         strncpy (device_public_ip, data, sizeof(device_public_ip));
     }
     // unsubscribe from events
@@ -23,35 +32,77 @@ void info_handler(const char *topic, const char *data) {
     }
 }
 
+// enable sysstem treading
+SYSTEM_THREAD(ENABLED);
+
+// manual mode
+SYSTEM_MODE(MANUAL);
+
 void setup() {
-    
+  
+  // turn wifi module on
+  WiFi.on();
+
+  // start serial
   Serial.begin(9600);
-  Time.zone(-6); // to get the time correctly
+  waitFor(Serial.isConnected, 5000); // give monitor 5 seconds to connect
   Serial.println("Starting up...");
+
+  // timezone
+  Time.zone(-6); // to get the time correctly
+
+  // led setup
   pinMode(led, OUTPUT);
+  digitalWrite(led, HIGH);
 
-  // device info
-  Serial.println("Requesting device info...");
+  // particle subscriptions
   Particle.subscribe("spark/", info_handler, ALL_DEVICES);
-  Particle.publish("spark/device/name", PRIVATE);
-  Particle.publish("spark/device/ip", PRIVATE);
-  Serial.println("Startup complete");
-
+  
+  // finished
+  Serial.println("Setup complete");
 }
 
 void loop() {
-  // To blink the LED, first we'll turn it on...
-  digitalWrite(led, HIGH);
-  
-  // We'll leave it on for x ms...
-  delay(interval);
 
-  // Then we'll turn it off...
-  digitalWrite(led, LOW);
- 
-  // Wait x ms...
-  delay(interval);
+  // blinker
+  if (millis() - timer > interval) {
+    if (on) {
+      // turn off
+      digitalWrite(led, LOW);
+      Serial.println(Time.format(Time.now(), "Turning LED off at %H:%M:%S %d.%m.%Y"));
+    } else {
+      // turn on
+      digitalWrite(led, HIGH);
+      Serial.println(Time.format(Time.now(), "Turning LED on at %H:%M:%S %d.%m.%Y"));
+    }
+    on = !on;
+    timer = millis();
+  }
 
-  // repeat
-  Serial.println(Time.format(Time.now(), "%H:%M:%S %d.%m.%Y"));
+  // cloud connection
+  if (Particle.connected()) {
+    if (!connected) {
+      // connection just made
+      WiFi.macAddress(mac);
+      Serial.printf("MAC address: %02x:%02x:%02x:%02x:%02x:%02x\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+      Serial.println(Time.format(Time.now(), "Cloud connection established at %H:%M:%S %d.%m.%Y"));
+      Particle.publish("spark/device/name", PRIVATE);
+      Particle.publish("spark/device/ip", PRIVATE);
+      connected = true;
+    }
+    Particle.process();
+  } else if (connected) {
+    // should be connected but isn't
+    Serial.println(Time.format(Time.now(), "Lost cloud connected at %H:%M:%S %d.%m.%Y"));
+    connect = false;
+    connected = false;
+  } else if (!connect) {
+    // start cloud connection
+    Serial.println(Time.format(Time.now(), "Initiate connection at %H:%M:%S %d.%m.%Y"));
+    Particle.connect();
+    connect = true;
+  }
+
+
+
 }
