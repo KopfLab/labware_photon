39fc25aa7bb119dec7afea3a607f3f453a434126
Sebastian Kopf <sebkopf>
2020-12-27 14:03:30 -0700

    introduce controller type logger component for easier derivation

---
+++ src/modules/logger/ControllerLoggerComponent.h
@@ -0,0 +1,19 @@
+/**
+ * This class is the basis for logger components that provide control over some peripheral but don't read data.
+ */
+
+#pragma once
+#include "LoggerComponent.h"
+#include "LoggerController.h"
+
+/* component */
+class ControllerLoggerComponent : public LoggerComponent
+{
+
+  public:
+
+    /*** constructors ***/
+    // these types of logger components don't usually have global time offsets --> set to false
+    ControllerLoggerComponent (const char *id, LoggerController *ctrl) : LoggerComponent(id, ctrl, false) {}
+
+};
