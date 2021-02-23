74a547a32c1fb41d8a90f148d8eb2f8da6ad5894
Sebastian Kopf <sebkopf>
2021-02-22 21:37:35 -0700

    use common serial reader constants

--- src/devices/chemglass_scale/ChemglassScaleLoggerComponent.h
+++ src/devices/chemglass_scale/ChemglassScaleLoggerComponent.h
@@ -9,14 +9,7 @@
 #define P_STABLE    -3 // [ S]
 #define P_BYTE       0 // anything > 0 is specific byte
 
-// specific ascii characters (actual byte values)
-#define B_SPACE      32 // \\s
-#define B_CR         13 // \r
-#define B_NL         10 // \n
-#define B_0          48 // 0
-#define B_9          57 // 9
-
-const int SCALE_DATA_PATTERN[] = {P_VAL, P_VAL, P_VAL, P_VAL, P_VAL, P_VAL, P_VAL, P_VAL, P_VAL, B_SPACE, B_SPACE, P_UNIT, P_STABLE, B_CR, B_NL};
+const int SCALE_DATA_PATTERN[] = {P_VAL, P_VAL, P_VAL, P_VAL, P_VAL, P_VAL, P_VAL, P_VAL, P_VAL, ' ', ' ', P_UNIT, P_STABLE, SERIAL_B_CR, SERIAL_B_NL};
 
 /*** component ***/
 class ChemglassScaleLoggerComponent : public ScaleLoggerComponent
@@ -31,7 +24,7 @@ class ChemglassScaleLoggerComponent : public ScaleLoggerComponent
             /* baud rate */             4800,
             /* serial config */         SERIAL_8N1,
             /* request command */       "#",
-            /* data pattern size */     sizeof(SCALE_DATA_PATTERN) / sizeof(SCALE_DATA_PATTERN[0]) - 1
+            /* data pattern size */     sizeof(SCALE_DATA_PATTERN) / sizeof(SCALE_DATA_PATTERN[0])
         ) {}
 
     /*** manage data ***/
@@ -48,8 +41,8 @@ void ChemglassScaleLoggerComponent::processNewByte() {
 
     // pattern interpretation
     char c = (char) new_byte;
-    if ( SCALE_DATA_PATTERN[data_pattern_pos] == P_VAL && ( (new_byte >= B_0 && new_byte <= B_9) || c == ' ' || c == '+' || c == '-' || c == '.') ) {
-        if (new_byte != B_SPACE) appendToSerialValueBuffer(new_byte); // value (ignoring spaces)
+    if ( SCALE_DATA_PATTERN[data_pattern_pos] == P_VAL && ( (new_byte >= SERIAL_B_0 && new_byte <= SERIAL_B_9) || c == ' ' || c == '+' || c == '-' || c == '.') ) {
+        if (c != ' ') appendToSerialValueBuffer(new_byte); // value (ignoring spaces)
     } else if (SCALE_DATA_PATTERN[data_pattern_pos] == P_UNIT && (c == 'G' || c == 'O' || c == 'C')) {
         // units
         if (c == 'G') setSerialUnitsBuffer("g"); // grams
